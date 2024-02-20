95f271a33c435a2864b2af6a49ce108a1a329492
mikoloism <mikoloism@gmail.com>
2024-02-20 14:25:02 +0330

    feat(bin): add Git prompt installer

--- bin/configs/Git/setup.sh
+++ bin/configs/Git/setup.sh
@@ -6,12 +6,17 @@ HERE=$(dirname $0)
 printf "${ICON} Git"
 
 if [[ ! $(command -v "git") ]]; then
-	printf "Git Command Not Exist"
-	printf "Installing Git..."
-	apt install git-all
+  printf "Git Command Not Exist"
+  printf "Installing Git..."
+  apt install git-all
 fi
 
 read -p "What is your USERNAME: " GIT_USERNAME
 read -p "What is your EMAIL ADDRESS: " GIT_EMAIL
 printf "Setup Git Configs into $HOME"
 ln -sf "$HERE/.gitconfig" "$HOME/.gitconfig"
+
+git config --global user.name "$GIT_USERNAME"
+git config --global user.email "$GIT_EMAIL"
+git config --global author.name "$GIT_USERNAME"
+git config --global author.email "$GIT_EMAIL"
