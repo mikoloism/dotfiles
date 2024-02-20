ba554cfe85e92120b74cc1733cad250f1e25a18c
mikoloism <mikoloism@gmail.com>
2024-02-21 00:25:34 +0330

    fix(repo): improve `path.sh`
    - add `path-contain` function to throw invalid arg
    - improve default argument

--- repository/.config/shell/path.sh
+++ repository/.config/shell/path.sh
@@ -1,40 +1,55 @@
 #!/usr/bin/env bash
 
 path-normilize() {
-	local RESULT=$(echo "$1" | sed "s/\$HOME/~/g")
-	local RESULT=$(echo "${RESULT/#$HOME/~}")
-	echo "$RESULT"
+    local RESULT=$(echo "$1" | sed "s/\$HOME/~/g" | sed -e "s|$HOME|~|g" -e "s|^$HOME|~|")
+    local RESULT=$(echo "${RESULT/#$HOME/~}")
+    echo "$RESULT"
+}
+
+path-contain() {
+    if [ -z "$1" ]; then
+        echo "Error: Arguments not provided." >&2
+        return 1
+    fi
+
+    local TARGET=$(path-normilize "$1")
+    if str-include "$PATH" "$TARGET"; then
+        echo "Error: $TARGET available." >&2
+        return 2
+    fi
 }
 
 path-use() {
-	[ -z "$1" ] && return 1
-	local NEWPATH=$(path-normilize "$1")
-	export PATH="$PATH:$NEWPATH"
+    path-contain "$@" || return
+    local NEWPATH=$(path-normilize "$1")
+    export PATH="$PATH:$NEWPATH"
 }
 
 path-add() {
-	[ -z "$1" ] && return 1
-	local NEWPATH=$(path-normilize "$1")
-	if [ "$2" = '--use' ]; then
+    path-contain "$@" || return
+
+    local NEWPATH=$(path-normilize "$1")
+    if [ "$2" = '--use' ]; then
         echo "path-use \"$NEWPATH\"" >> "~/.config/shell/path-source.sh"
-	else
-		echo "export PATH=\"\$PATH:$NEWPATH\"" >> "~/.config/shell/path-source.sh"
-	fi
+    else
+        echo "export PATH=\"\$PATH:$NEWPATH\"" >> "~/.config/shell/path-source.sh"
+    fi
 }
 
 path-list() {
-	local PARAMS="${1:-\n}"
-	local TARGET=$(path-normilize "$PARAMS")
-	echo "$PATH" | tr ":" "\n" | grep "$TARGET" --line-number --initial-tab --ignore-case --color="always"
+    local PARAMS="${1:-\\n}"
+    local TARGET=$(path-normilize "$PARAMS")
+    echo "$PATH" | tr ":" "\n" | grep "$TARGET" --line-number --initial-tab --ignore-case --color="always"
 }
 
 path-count() {
-	local PARAMS="${1:-\n}"
-	local TARGET=$(path-normilize "$PARAMS")
-	local COUNT=$(echo "$PATH" | tr ":" "\n" | grep "$TARGET" --line-number --initial-tab --ignore-case --color="always" --count)
-	echo "$COUNT"
+    local PARAMS="${1:-\\n}"
+    local TARGET=$(path-normilize "$PARAMS")
+    local COUNT=$(echo "$PATH" | tr ":" "\n" | grep "$TARGET" --line-number --initial-tab --ignore-case --color="always" --count)
+    echo "$COUNT"
 }
 
 path-remove() {
-	return 1
+    echo "Error: not implemented" >&2
+    return 3
 }
