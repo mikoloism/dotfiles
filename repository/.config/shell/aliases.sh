2544a547431b9a13c00b65935ae4866fe9c6553e
mikoloism <mikoloism@gmail.com>
2024-02-22 11:54:35 +0330

    refactor(repo): improve `rm` aliases
    make `verbose` by default for `rm` command,
    to remove silent(quit) should use `rms`

--- repository/.config/shell/aliases.sh
+++ repository/.config/shell/aliases.sh
@@ -7,9 +7,10 @@ alias mv="mv --interactive --verbose"
 alias ln="ln --interactive --verbose --symbolic"
 
 ### `rm` command
-alias rm="rm -i --recursive"
+alias rms="rm -i --recursive"
 alias rmv="rm -i --recursive --verbose"
 alias rmf="rmv --force"
+alias rm="rm -i --recursive --verbose"
 ### `rm` end
 
 ### `mkdir` command
