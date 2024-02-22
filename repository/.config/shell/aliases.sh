7f19c5f5feedbd6a6b4d8547013c80c74721e9e5
mikoloism <mikoloism@gmail.com>
2024-02-22 11:18:14 +0330

    feat(repo): add more`aliases.sh`
    include:
    - `cp` command
    - `mv` command
    - `rm` command
    - `mkdir` command

--- repository/.config/shell/aliases.sh
+++ repository/.config/shell/aliases.sh
@@ -2,3 +2,17 @@ alias dof='/usr/bin/git --git-dir=$HOME/.dogfile/ --work-tree=$HOME'
 alias ping="ping -w 10 -i 0.250 -c 16 "
 alias lzg="lazygit"
 alias l="ls -la --group-directories-first --sort=extension"
+alias cp="cp --interactive --verbose"
+alias mv="mv --interactive --verbose"
+
+### `rm` command
+alias rm="rm -i --recursive"
+alias rmv="rm -i --recursive --verbose"
+alias rmf="rmv --force"
+### `rm` end
+
+### `mkdir` command
+alias mkdir="mkdir --parents --verbose"
+alias mkd="mkdir"
+alias md="mkdir"
+### `mkdir` end
