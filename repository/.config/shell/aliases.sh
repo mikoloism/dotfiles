13e1b8db124a40bc0f0c0c1d3f64ecabb819a630
mikoloism <mikoloism@gmail.com>
2024-02-23 16:45:11 +0330

    feat(repo): update `aliases.sh` to configure `ls` and `exa`

--- repository/.config/shell/aliases.sh
+++ repository/.config/shell/aliases.sh
@@ -1,10 +1,28 @@
 alias dof='/usr/bin/git --git-dir=$HOME/.dogfile/ --work-tree=$HOME'
 alias ping="ping -w 10 -i 0.250 -c 16 "
 alias lzg="lazygit"
-alias l="ls -la --group-directories-first --sort=extension"
 alias cp="cp --interactive --verbose"
 alias mv="mv --interactive --verbose"
 alias ln="ln --interactive --verbose --symbolic"
+
+
+### `ls` and `exa` (list directory content)
+# [LEGACY] alias l='ls -la --group-directories-first --sort=version'
+alias exa='command exa --colour="auto" --colour-scale --group-directories-first --binary --header --sort="name" --no-time'
+alias ls='exa'
+alias la='exa --all'
+alias l='la'
+alias lc='la --classify'
+alias lt="la --level=1 --tree"
+alias ltc="la --level=1 --tree --classify"
+alias lg="ls --group"
+alias lgc="ls --group --classify"
+alias lga="la --group"
+alias ll="ls --oneline"
+alias lla="ll --all"
+alias ld='ls --only-dirs'
+alias lda='ld --all'
+alias lld='ld --all'
 
 ### `rm` command
 alias  rm="rmv"
