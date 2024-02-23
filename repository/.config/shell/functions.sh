57708f30eea47109082aaf75c9801612cb8dd609
mikoloism <mikoloism@gmail.com>
2024-02-23 16:43:21 +0330

    feat(repo): add `edit` and `editor` to `functions
    path changed: `.config/shell/functions.sh`
    
    added:
    - `edit`   to open `$EDITOR`
    - `eidtor` to configure `$EDITOR`

--- repository/.config/shell/functions.sh
+++ repository/.config/shell/functions.sh
@@ -18,3 +18,25 @@ shagen() {
 	echo "$(echo -n "$passphrase" | shasum -a "$algorithm" --text)"
 }
 
+edit() {
+  $EDITOR "$@"
+}
+
+editor() {
+  if [ -z "$1" ]; then
+    echo "eidtor: $EDITOR"
+
+    if [ -z "$EDITOR" ]; then
+      return 0
+    else
+      return 1
+    fi
+    else
+    if [ -z "$2" ]; then
+      export EDITOR="$1"
+      return 0
+    else
+      echo "export EDITOR=$1" >> "$HOME/.zshrc"
+    fi
+  fi
+}
